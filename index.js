3da6232ec0a3054d2b0049ac9066b75f9b884340
ogzacky75 <ogzacky75@gmail.com>
2025-06-16 02:33:42 +0300

    adding challange

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener ('DOMContentLoaded', ()=>{
+	let form = document.querySelector('form')
+	form.addEventListener('submit', (sub)=>{
+		sub.preventDefault()
+		buildList(sub.target.input.value)
+		form.reset()
+	})
+})
+
+function buildList(List){
+
+	let li = document.createElement('ol')
+	let btn = document.createElement('button')
+    btn.addEventListener('click', removeName)
+	btn.textContent = 'Remove'
+	li.textContent = List
+	li.appendChild(btn)
+	document.querySelector('#list').appendChild(li)
+}
+
+function removeName(r){
+	r.target.parentNode.remove()
+}
